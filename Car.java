28172e9ecba7dde6e0e40cf90e266693396591e5
Zenoson <zenoson1234@gmail.com>
2023-03-08 08:19:00 -0500

    added methods for car

--- Car.java
+++ Car.java
@@ -1,3 +1,44 @@
+import java.util.List;
+
 public abstract class Car {
-    
+    public Car(String make, String model, double startingMileage) {
+
+    }
+
+    public Car(String make, String model) {
+        this(make, model, 0);
+    }
+
+    public boolean canDrive(double miles) {
+        return true;
+    }
+
+    public abstract void drive(double miles);
+
+    @Override
+    public String toString() {
+        return "";
+    }
+
+    public double getMileage() {
+        return 1.0;
+    }
+
+    public String getMake() {
+        return "hi";
+    }
+
+    public String getModel() {
+        return "hi";
+    }
+
+    public abstract double getRemainingRange();
+
+    protected void addMileage(double miles) {
+
+    }
+
+    public int roadTrip(List<Double> milesEachDay) {
+        return 0;
+    }
 }
