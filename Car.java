2afe6a7775e5e86b363c0c636e79f807deb4ec50
Zenoson <zenoson1234@gmail.com>
2023-03-08 08:41:46 -0500

    implemented more

--- Car.java
+++ Car.java
@@ -23,7 +23,7 @@ public abstract class Car {
 
     @Override
     public String toString() {
-        return "";
+        return String.format("%s $s ($.1f mi)", make, model, mileage);
     }
 
     public double getMileage() {
