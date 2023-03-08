efc49dc9e78669a050e7a7b8406c8845eb289386
niclei25 <nikaleifer@gmail.com>
2023-03-08 08:42:16 -0500

    electriccar, chevy, ford, tesla methods

--- ElectricCar.java
+++ ElectricCar.java
@@ -1,3 +1,43 @@
-public class ElectricCar {
-    
+public class ElectricCar extends Car {
+    /** Note: Car begins with a full charge (and thus range).
+    @throws IllegalArgumentException if milesOnMaxCharge is nonpositive.*/
+    public ElectricCar(String make, String model, double startingMileage,
+    double milesOnMaxCharge) {
+
+    }
+    /** Defaults mileage to 0.
+    @throws IllegalArgumentException if milesOnMaxCharge is nonpositive.*/
+    public ElectricCar (String make, String model, double
+    milesOnMaxCharge) {
+
+    }
+
+    /** Drives the full given number of miles.
+    @throws IllegalArgumentException if miles is negative.
+    @throws IllegalArgumentException if miles is too high given the
+    current charge.*/
+    public void drive(double miles) {
+
+    }
+
+    /** Returns how many more miles the car can currently go without
+    recharging. */
+    public double getRemainingRange() {
+        return 0;
+    }
+
+    /** Returns how many miles the car could go on a full charge. */
+    public double getMaxRange() {
+        return 0;
+    }
+
+    /** Recharges the car to max range capability. */
+    public void recharge() {
+
+    }
+
+    /** Decreases the amount of energy in the battery based by the number
+    of miles passed as an argument. */
+    protected void decreaseCharge(double miles) {
+    }
 }
